24eecd577a2823747d212a37d9a0cb05129f2fd8
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2012-07-15 19:11:16 +0200

    Added new class StringExtensions and the helper method ToFixedLength

---
+++ src/shell.me/ShellMe.CommandLine/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace ShellMe.CommandLine.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToFixedLength(this string text, int length)
+        {
+            if (string.IsNullOrEmpty(text))
+                text = string.Empty;
+
+            var missingCharsCount = length - text.Length >= 0 ? length - text.Length : 0;
+
+            return new string(
+                 text.ToCharArray()
+                     .Concat(Enumerable.Repeat(' ', missingCharsCount))
+                     .Take(length)
+                     .ToArray());
+        }
+    }
+}
